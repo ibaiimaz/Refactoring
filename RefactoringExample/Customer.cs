e0c8925bb9f7f1498789db5e25ffa70662334260
ibaiimaz <ibaiimaz@gmail.com>
2014-11-12 10:35:32 +0100

    First commit before start with Polymorphism

---
+++ RefactoringExample/Customer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace RefactoringExample
+{
+    public class Customer
+    {
+        private String _name;
+        private List<Rental> _rentals = new List<Rental>();
+
+        public Customer(String name)
+        {
+            _name = name;
+        }
+
+        public void addRental(Rental arg)
+        {
+            _rentals.Add(arg);
+        }
+
+        public String getName()
+        {
+            return _name;
+        }
+
+        public String statement()
+        {
+            String result = "Rental Record for " + getName() + "\n";
+            foreach (Rental each in _rentals)
+            {
+                //show figures for this rental
+                result += "\t" + each.getMovie().getTitle() + "\t" + each.GetCharge().ToString() + "\n";
+            }
+            //add footer lines
+            result += "Amount owed is " + GetTotalCharge().ToString() +
+            "\n";
+            result += "You earned " + GetTotalFrequentRenterPoints().ToString()
+            +
+            " frequent renter points";
+            return result;
+        }
+
+        private double GetTotalCharge()
+        {
+            double result = 0;
+            foreach (Rental each in _rentals)
+            {
+                result += each.GetCharge();
+            }
+            return result;
+        }
+
+        private double GetTotalFrequentRenterPoints()
+        {
+            double result = 0;
+            foreach (Rental each in _rentals)
+            {
+                result += each.GetFrecuentRenterPoints();
+            }
+            return result;
+        }
+
+        public String htmlStatement()
+        {
+
+            String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+            foreach (Rental each in _rentals)
+            {
+                //show figures for each rental
+                result += each.getMovie().getTitle() + ": " + each.GetCharge().ToString() + "<BR>\n";
+            }
+            //add footer lines
+            result += "<P>You owe <EM>" + GetTotalCharge().ToString() + "</EM><P>\n";
+            result += "On this rental you earned <EM>" + GetTotalFrequentRenterPoints().ToString() + "</EM> frequent renter points<P>";
+            return result;
+        }
+    }
+}
---
+++ RefactoringExample/Movie.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace RefactoringExample
+{
+    public class Movie
+    {
+        public const int CHILDRENS = 2;
+        public const int REGULAR = 0;
+        public const int NEW_RELEASE = 1;
+        private String _title;
+        private int _priceCode;
+
+        public Movie(String title, int priceCode)
+        {
+            _title = title;
+            _priceCode = priceCode;
+        }
+
+        public int getPriceCode()
+        {
+            return _priceCode;
+        }
+
+        public void setPriceCode(int arg)
+        {
+            _priceCode = arg;
+        }
+
+        public String getTitle()
+        {
+            return _title;
+        }
+
+        public double GetCharge(int daysRented)
+        {
+            double result = 0;
+            switch (getPriceCode())
+            {
+                case Movie.REGULAR:
+                    result += 2;
+                    if (daysRented > 2)
+                        result += (daysRented - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    result += daysRented * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    result += 1.5;
+                    if (daysRented > 3)
+                        result += (daysRented - 3) * 1.5;
+                    break;
+            }
+            return result;
+        }
+
+        public int GetFrecuentRenterPoints(int daysRented)
+        {
+            // add bonus for a two day new release rental
+            if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+                return 2;
+            else
+                return 1;
+        }
+    }
+}
